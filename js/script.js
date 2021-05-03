739684ffa8feb5a68c5e14bdd31a8781d73d798e
Barłomiej Tabero <bartekt92@gmail.com>
2021-05-03 11:42:32 +0200

    refactoring currency page

--- js/script.js
+++ js/script.js
@@ -1,72 +1,134 @@
-console.log("Witajcie programiści!")
-let amount = document.querySelector(".js-amount");
-let currency = document.querySelector(".js-currency");
-let currency2 = document.querySelector(".js-currency2");
-let rate = document.querySelector(".js-rate");
-let result = document.querySelector(".js-result");
-let form = document.querySelector(".js-form");
-let value1 = document.querySelector(".js-value1");
-let value2 = document.querySelector(".js-value2");
-let alert = document.querySelector(".js-alert");
-let infoAlert = document.querySelector(".js-infoAlert");
-let button = document.querySelector(".js-button");
-let x = 0;
-let kurs = rate.value;
-let a;
-let r = (amount.value) * 4.56;
-result.value = r
-infoAlert.innerHTML = `<strong class="form__strong"> ${amount.value} ${currency.value} </strong>to w przeliczeniu: <strong class="form__strong"> ${result.value} ${currency2.value} </strong>`;
+{
+const init = () => {
+    const infoAlert = document.querySelector(".js-infoAlert");
+    const result = document.querySelector(".js-result");
+    console.log("Witajcie programiści!");
+    result.value = 4.56
+    infoAlert.innerHTML = `<strong class="form__strong"> 1 € </strong>to w przeliczeniu: <strong class="form__strong"> 4.56 zł </strong>`;
+};
+init();
 
-button.addEventListener("click", () => {
-    a = currency.value;
-    currency.value = currency2.value
-    currency2.value = a;
-});
-
-form.addEventListener("input", () => {
-    rate.addEventListener("input", () => {
-        x = 1;
-    });
-    currency.addEventListener("input", () => {
-        x = 0;
-    });
-    currency.addEventListener("input", () => {
-        rate.value = ""
-    });
+const result = document.querySelector(".js-result");
+const amount = document.querySelector(".js-amount");
+let firstCurrency = document.querySelector(".js-currency");
+let secondCurrency = document.querySelector(".js-currency2");
+const rateElement = document.querySelector(".js-rate");
+const form = document.querySelectorAll(".js-label");
+const button = document.querySelector(".js-button");
+let enteredRate = 0;
+let rate = rateElement.value;
+/*
+function rateValueReset(){
+    enteredRate = 0;
+    rateElement.value = "";
+}   */
+const changeRatesOnClick = () => {
     alert.innerText = "";
-    switch (currency.value) {
-        case "zł":
-            kurs = (x === 1) ? rate.value : 0.22;
-            if (currency.value === currency2.value) {
-                currency2.value = "€";
-            } else if (currency2.value === "$") {
-                kurs = (x === 1) ? rate.value : 0.26;
-            }
-            break;
-        case "€":
-            kurs = (x === 1) ? rate.value : 4.56;
-            if (currency.value === currency2.value) {
-                currency2.value = "zł";
-            } else if (currency2.value === "$") {
-                kurs = (x === 1) ? rate.value : 1.21;
-            }
-            break;
-        case "$":
-            kurs = (x === 1) ? rate.value : 3.78;
-            if (currency.value === currency2.value) {
-                currency2.value = "zł";
-                alert.innerText = `Chciałeś zamienić Dolary na Dolary??? Masz ten amerykanski feeling i vibe 😆`;
-            } else if (currency2.value === "€") {
-                kurs = (x === 1) ? rate.value : 0.83;
-            }
-            break;
-    }
-    rate.placeholder = kurs;
-    console.log(x);
-    r = (amount.value) * kurs;
-    result.value = r.toFixed(2);
-    value1.innerText = currency.value;
-    value2.innerText = currency2.value;
-    infoAlert.innerHTML = `<strong class="form__strong"> ${amount.value} ${currency.value} </strong>to w przeliczeniu: <strong class="form__strong"> ${result.value} ${currency2.value} </strong>`;
-});
+    let substituteCurrency = firstCurrency.value;
+    firstCurrency.value = secondCurrency.value;
+    secondCurrency.value = substituteCurrency;
+    enteredRate = 0;
+    rateElement.value = "";
+    checkRate(firstCurrency, rateElement);
+    logResult(rate, firstCurrency, secondCurrency, rateElement);
+};
+button.addEventListener("click", changeRatesOnClick);
 
+function checkCurrency(firstCurrency, secondCurrency) {
+    const alert = document.querySelector(".js-alert");
+    alert.innerText = "";
+    if (firstCurrency.value === secondCurrency.value) {
+        switch (firstCurrency.value) {
+            case "zł": secondCurrency.value = "€";
+                break;
+            case "€": secondCurrency.value = "zł";
+                break;
+            case "$": secondCurrency.value = "zł";
+                alert.innerText = `Chciałeś zamienić Dolary na Dolary??? Masz ten amerykanski feeling i vibe 😆`;
+                break;
+        }
+    }
+};
+function checkCurrency2(firstCurrency, secondCurrency) {
+    const alert = document.querySelector(".js-alert");
+    alert.innerText = "";
+    if (firstCurrency.value === secondCurrency.value) {
+        switch (secondCurrency.value) {
+            case "zł": firstCurrency.value = "€";
+                break;
+            case "€": firstCurrencyvalue = "zł";
+                break;
+            case "$": firstCurrency.value = "zł";
+                alert.innerText = `Chciałeś zamienić Dolary na Dolary??? Masz ten amerykanski feeling i vibe 😆`;
+                break;
+        }
+    }
+};
+function checkRate(firstCurrency, rateElement) {
+    switch (firstCurrency.value) {
+        case "zł":
+            if (secondCurrency.value === "$") {
+                return rate = (enteredRate === 1) ? rateElement.value : 0.26;
+            } else {
+                return rate = (enteredRate === 1) ? rateElement.value : 0.22;
+            }
+        case "€":
+
+            if (secondCurrency.value === "$") {
+                return rate = (enteredRate === 1) ? rateElement.value : 1.21;
+            } else {
+                return rate = (enteredRate === 1) ? rateElement.value : 4.56;
+            }
+
+        case "$":
+
+            if (secondCurrency.value === "€") {
+                return rate = (enteredRate === 1) ? rateElement.value : 0.83;
+            }
+            else {
+                return rate = (enteredRate === 1) ? rateElement.value : 3.78;
+            }
+    }
+};
+function calculateResult() {
+    r = (amount.value) * rate;
+    return result.value = r.toFixed(2);
+};
+function logResult(rate, firstCurrency, secondCurrency, rateElement) {
+    const result = document.querySelector(".js-result");
+    const value1 = document.querySelector(".js-value1");
+    const value2 = document.querySelector(".js-value2");
+    const infoAlert = document.querySelector(".js-infoAlert");
+    rateElement.placeholder = rate;
+    calculateResult();
+    value1.innerText = firstCurrency.value;
+    value2.innerText = secondCurrency.value;
+    infoAlert.innerHTML = `<strong class="form__strong"> ${amount.value} ${firstCurrency.value} </strong>to w przeliczeniu: <strong class="form__strong"> ${result.value} ${secondCurrency.value} </strong>`;
+};
+
+function listenInputAndDo(){
+    firstCurrency.addEventListener("input", () => {
+    enteredRate = 0;
+    rateElement.value = "";
+    checkCurrency(firstCurrency, secondCurrency);
+})
+secondCurrency.addEventListener("input", () => {
+    checkCurrency2(firstCurrency, secondCurrency);
+})
+
+rateElement.addEventListener("input", () => {
+    enteredRate = 1;
+    checkRate(firstCurrency, rateElement);
+    logResult(rate, firstCurrency, secondCurrency, rateElement);
+});
+form.forEach(element => {
+    element.addEventListener("input", () => {
+
+        alert.innerText = "";
+        checkRate(firstCurrency, rateElement);
+        logResult(rate, firstCurrency, secondCurrency, rateElement);
+    });
+})
+};
+listenInputAndDo();
+}
